6c04e133152c0154fffb9cca74d9b9914e2e9386
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-21 20:59:44 -0300

    feat(test): fase 3 -> add suporte ao metodo de pagamento pix

--- test_fluxo_pagamento.py
+++ test_fluxo_pagamento.py
@@ -6,42 +6,42 @@ class TestFluxoPagamento(TestCase):
     def test_nao_deve_processar_pagamento_com_saldo_insuficiente(self):
         pagamento = Pagamento()
         saldo_insuficiente = 10
-        self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_insuficiente, None)
+        self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_insuficiente, "pix")
     
 
     def test_deve_processar_pagamento_com_saldo_suficiente(self):
         pagamento = Pagamento()
         saldo_suficiente = 1000
-        resultado = pagamento.processar_pagamento(100, saldo_suficiente, None)
+        resultado = pagamento.processar_pagamento(100, saldo_suficiente, "pix")
         self.assertTrue(resultado)
 
 
     def test_nao_deve_processar_pagamento_com_tipo_invalido(self):
         pagamento = Pagamento()
-        self.assertRaises(TypeError, pagamento.processar_pagamento, "cem", 500, None)
+        self.assertRaises(TypeError, pagamento.processar_pagamento, "cem", 500, "pix")
 
 
     def test_nao_deve_processar_pagamento_com_valor_negativo(self):
         pagamento = Pagamento()
-        self.assertRaises(ValueError, pagamento.processar_pagamento, -10, 500, None)
+        self.assertRaises(ValueError, pagamento.processar_pagamento, -10, 500, 'pix')
 
 
     def test_nao_deve_processar_pagamento_com_valor_zero(self):
         pagamento = Pagamento()
-        self.assertRaises(ValueError, pagamento.processar_pagamento, 0, 500, None)
+        self.assertRaises(ValueError, pagamento.processar_pagamento, 0, 500, "pix")
 
 
     def  test_nao_deve_processar_pagamento_com_saldo_zerado(self):
         pagamento = Pagamento()
         saldo_zerado = 0
 
-        self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_zerado, None)
+        self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_zerado, "pix")
 
     
     def test_nao_deve_processar_pagamento_com_saldo_informado_de_tipo_invalido(self):
         pagamento = Pagamento()
 
-        self.assertRaises(TypeError, pagamento.processar_pagamento, 100, "quinhentos", None)
+        self.assertRaises(TypeError, pagamento.processar_pagamento, 100, "quinhentos", "pix")
 
     
     #“o sistema ainda não suporta método de pagamento”
