4551e456fdf0b4e1545eeec5871bf66c9d270645
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-21 20:13:03 -0300

    feat: testes para implementar melhorias nas validações no processar pagamento

--- pagamento.py
+++ pagamento.py
@@ -1,18 +1,18 @@
 class Pagamento:
 
-    def processar_pagamento(self, limite):
-        if not isinstance(limite, (float, int)):
-            raise TypeError("limite deve ser um numero")
-
-
-        limiteMax = 100000
-        if limite > limiteMax:
-            raise ValueError("limite máximo excedido")
-
-
-        if limite <= 0:
-            raise ValueError("sem limite pra processar pagamento")
+    def processar_pagamento(self, valor_pagamento, saldo_disponivel):
+        if not isinstance(valor_pagamento, (float, int)):
+            raise TypeError("valor do pagamento não é um numero")
         
+        if not isinstance(saldo_disponivel, (float, int)):
+            raise TypeError("saldo disponível não é um número")
+
+        if valor_pagamento <= 0:
+            raise ValueError("valor do pagamento não pode ser negativo ou 0")
+        
+        if valor_pagamento > saldo_disponivel:
+            raise ValueError("saldo insuficiente para o pagamento")        
+
         
         return True
     
--- test_fluxo_pagamento.py
+++ test_fluxo_pagamento.py
@@ -2,50 +2,45 @@ from unittest import TestCase
 from pagamento import Pagamento
 
 class TestFluxoPagamento(TestCase):
-            
-    def test_deve_processar_pagamento_com_sucesso(self):
+        
+    def test_nao_deve_processar_pagamento_com_saldo_insuficiente(self):
         pagamento = Pagamento()
+        saldo_insuficiente = 10
+        self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_insuficiente)
+    
 
-        resultado = pagamento.processar_pagamento(100)
-
+    def test_deve_processar_pagamento_com_saldo_suficiente(self):
+        pagamento = Pagamento()
+        saldo_suficiente = 1000
+        resultado = pagamento.processar_pagamento(100, saldo_suficiente)
         self.assertTrue(resultado)
+
+
+    def test_nao_deve_processar_pagamento_com_tipo_invalido(self):
+        pagamento = Pagamento()
+        self.assertRaises(TypeError, pagamento.processar_pagamento, "cem", 500)
 
 
     def test_nao_deve_processar_pagamento_com_valor_negativo(self):
         pagamento = Pagamento()
+        self.assertRaises(ValueError, pagamento.processar_pagamento, -10, 500)
 
-        self.assertRaises(ValueError, pagamento.processar_pagamento, -10)
 
-    def test_nao_deve_processar_pagamento_com_valor_zerado(self):
+    def test_nao_deve_processar_pagamento_com_valor_zero(self):
         pagamento = Pagamento()
+        self.assertRaises(ValueError, pagamento.processar_pagamento, 0, 500)
 
-        self.assertRaises(ValueError, pagamento.processar_pagamento, 0)
 
-    def test_deve_processar_pagamento_com_valor_quebrado(self):
+    def  test_nao_deve_processar_pagamento_com_saldo_zerado(self):
         pagamento = Pagamento()
+        saldo_zerado = 0
 
-        resultado = pagamento.processar_pagamento(99.90)
-
-        self.assertTrue(resultado)
-
-    def test_deve_processar_pagamento_com_valor_minimo_permitido(self):
-        pagamento = Pagamento()
-
-        resultado = pagamento.processar_pagamento(0.01)
-
-        self.assertTrue(resultado)
-
-    def test_nao_deve_processar_pagamento_acima_do_teto_maximo(self):
-        pagamento = Pagamento()
-        valor_acima_teto = 100000.01
-
-        self.assertRaises(ValueError, pagamento.processar_pagamento, valor_acima_teto)
+        self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_zerado)
 
 
-    def test_nao_deve_processar_com_tipo_invalido(self):
-        pagamento = Pagamento()
-
-        self.assertRaises(TypeError, pagamento.processar_pagamento, "cem")
-        
 
     
+    def test_nao_deve_processar_pagamento_com_saldo_informado_de_tipo_invalido(self):
+        pagamento = Pagamento()
+
+        self.assertRaises(TypeError, pagamento.processar_pagamento, 100, "quinhentos")
