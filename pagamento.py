bac7c98e33588a3d2e6b706be747409099868ae3
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-21 01:11:08 -0300

    feat: caso pagamento com valor quebrado, com valor minimo e valor maximo permitido

--- pagamento.py
+++ pagamento.py
@@ -4,6 +4,12 @@ class Pagamento:
         if not isinstance(limite, (float, int)):
             raise TypeError("limite deve ser um numero")
 
+
+        limiteMax = 100000
+        if limite > limiteMax:
+            raise ValueError("limite máximo excedido")
+
+
         if limite <= 0:
             raise ValueError("sem limite pra processar pagamento")
         
--- test_fluxo_pagamento.py
+++ test_fluxo_pagamento.py
@@ -10,6 +10,7 @@ class TestFluxoPagamento(TestCase):
 
         self.assertTrue(resultado)
 
+
     def test_nao_deve_processar_pagamento_com_valor_negativo(self):
         pagamento = Pagamento()
 
@@ -19,6 +20,26 @@ class TestFluxoPagamento(TestCase):
         pagamento = Pagamento()
 
         self.assertRaises(ValueError, pagamento.processar_pagamento, 0)
+
+    def test_deve_processar_pagamento_com_valor_quebrado(self):
+        pagamento = Pagamento()
+
+        resultado = pagamento.processar_pagamento(99.90)
+
+        self.assertTrue(resultado)
+
+    def test_deve_processar_pagamento_com_valor_minimo_permitido(self):
+        pagamento = Pagamento()
+
+        resultado = pagamento.processar_pagamento(0.01)
+
+        self.assertTrue(resultado)
+
+    def test_nao_deve_processar_pagamento_acima_do_teto_maximo(self):
+        pagamento = Pagamento()
+        valor_acima_teto = 100000.01
+
+        self.assertRaises(ValueError, pagamento.processar_pagamento, valor_acima_teto)
 
 
     def test_nao_deve_processar_com_tipo_invalido(self):
