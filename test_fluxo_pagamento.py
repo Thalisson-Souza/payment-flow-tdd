807309d9c507bdcbe65ad6cee348cddffc69defe
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-21 22:40:16 -0300

    feat: add tests se tem taxa ou não no método de pagamento escolhido

--- test_fluxo_pagamento.py
+++ test_fluxo_pagamento.py
@@ -5,36 +5,41 @@ class TestFluxoPagamento(TestCase):
         
     def test_nao_deve_processar_pagamento_com_saldo_insuficiente(self):
         pagamento = Pagamento()
+        
         saldo_insuficiente = 10
         self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_insuficiente, "pix")
     
 
     def test_deve_processar_pagamento_com_saldo_suficiente(self):
         pagamento = Pagamento()
+
         saldo_suficiente = 1000
         resultado = pagamento.processar_pagamento(100, saldo_suficiente, "pix")
-        self.assertTrue(resultado)
+        self.assertEqual(resultado["valor_total"], 100)
 
 
     def test_nao_deve_processar_pagamento_com_tipo_invalido(self):
         pagamento = Pagamento()
+
         self.assertRaises(TypeError, pagamento.processar_pagamento, "cem", 500, "pix")
 
 
     def test_nao_deve_processar_pagamento_com_valor_negativo(self):
         pagamento = Pagamento()
+
         self.assertRaises(ValueError, pagamento.processar_pagamento, -10, 500, 'pix')
 
 
     def test_nao_deve_processar_pagamento_com_valor_zero(self):
         pagamento = Pagamento()
+
         self.assertRaises(ValueError, pagamento.processar_pagamento, 0, 500, "pix")
 
 
     def  test_nao_deve_processar_pagamento_com_saldo_zerado(self):
         pagamento = Pagamento()
-        saldo_zerado = 0
 
+        saldo_zerado = 0
         self.assertRaises(ValueError, pagamento.processar_pagamento, 200, saldo_zerado, "pix")
 
     
@@ -44,12 +49,12 @@ class TestFluxoPagamento(TestCase):
         self.assertRaises(TypeError, pagamento.processar_pagamento, 100, "quinhentos", "pix")
 
     
-    #“o sistema ainda não suporta método de pagamento”
+    # testes para implementação dos 3 tipos de pagamento
     def test_deve_processar_pagamento_quando_metodo_for_pix(self):
         pagamento = Pagamento()
 
         resultado = pagamento.processar_pagamento(100, 500, "pix")
-        self.assertTrue(resultado)
+        self.assertEqual(resultado["valor_total"], 100)
 
     def test_nao_deve_processar_pagamento_quando_metodo_for_invalido(self):
         pagamento = Pagamento()
@@ -60,10 +65,32 @@ class TestFluxoPagamento(TestCase):
         pagamento = Pagamento()
 
         resultado = pagamento.processar_pagamento(100, 500, "crédito")
-        self.assertTrue(resultado)
+        self.assertEqual(resultado["valor_total"], 105)
+
 
     def test_deve_processar_pagamento_quando_metodo_for_debito(self):
         pagamento = Pagamento()
 
         resultado = pagamento.processar_pagamento(100, 500, "débito")
-        self.assertTrue(resultado)
+        self.assertEqual(resultado["valor_total"], 100)
+
+
+    def test_nao_deve_ter_taxa_quando_pagamento_for_por_pix(self):
+        pagamento = Pagamento()
+
+        resultado = pagamento.processar_pagamento(100, 500, "pix")
+        self.assertEqual(resultado["valor_total"], 100)
+
+
+    def test_nao_deve_ter_taxa_quando_pagamento_for_por_debito(self):
+        pagamento = Pagamento()
+
+        resultado = pagamento.processar_pagamento(100, 500, "débito")
+        self.assertEqual(resultado["valor_total"], 100)
+
+
+    def test_deve_ter_taxa_quando_pagamento_for_por_credito(self):
+        pagamento = Pagamento()
+
+        resultado = pagamento.processar_pagamento(100, 500, "crédito")
+        self.assertEqual(resultado["valor_total"], 105)
