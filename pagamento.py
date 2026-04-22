a5fcfd3c8a3da84d890c51424d749eb4a5ac6328
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-21 21:12:46 -0300

    feat: validação metodo pix

--- pagamento.py
+++ pagamento.py
@@ -1,6 +1,9 @@
 class Pagamento:
 
-    def processar_pagamento(self, valor_pagamento, saldo_disponivel):
+    
+
+
+    def processar_pagamento(self, valor_pagamento, saldo_disponivel, metodo_pagamento):
         if not isinstance(valor_pagamento, (float, int)):
             raise TypeError("valor do pagamento não é um numero")
         
@@ -9,6 +12,11 @@ class Pagamento:
 
         if valor_pagamento <= 0:
             raise ValueError("valor do pagamento não pode ser negativo ou 0")
+        
+
+        if metodo_pagamento != "pix":
+            raise ValueError("modalidade nao aceita agora...")
+
         
         if valor_pagamento > saldo_disponivel:
             raise ValueError("saldo insuficiente para o pagamento")        
